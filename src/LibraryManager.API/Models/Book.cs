8e5c4a613abb3fbd753b3c89d56c649ee745dc4a
Renan Martins <renanmartins1999@hotmail.com>
2023-12-28 20:46:32 -0300

    [FEAT] Adding navigation properties to entities

--- src/LibraryManager.API/Models/Book.cs
+++ src/LibraryManager.API/Models/Book.cs
@@ -3,9 +3,11 @@ namespace LibraryManager.API.Models
     public class Book
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
         public string? Title { get; set; }
         public string? Author { get; set; }
         public string? ISBN { get; set; }
         public int PublishYear { get; set; }
+        public User? User { get; set; }
     }
 }
