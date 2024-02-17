ab2e5257bb7b4c35c81b1a3416ff910d1167fdf0
Renan Martins <renanmartins1999@hotmail.com>
2024-02-16 21:54:21 -0300

    [FEAT] Creating book view model

---
+++ src/LibraryManager.API/ViewModels/CreateBookViewModel.cs
@@ -0,0 +1,24 @@
+using LibraryManager.API.Models;
+
+namespace LibraryManager.API.ViewModels
+{
+    public class CreateBookViewModel
+    {
+        public int UserId { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? ISBN { get; set; }
+        public int PublishYear { get; set; }
+
+        public static Book ToEntity(CreateBookViewModel model)
+        {
+            return new Book {
+                UserId = model.UserId,
+                Title = model.Title,
+                Author = model.Author,
+                ISBN = model.ISBN,
+                PublishYear = model.PublishYear           
+            };
+        }
+    }
+}
