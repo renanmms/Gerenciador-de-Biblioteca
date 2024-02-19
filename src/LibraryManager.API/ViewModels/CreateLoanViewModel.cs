4f0f45da997feedb466b7182739bb3192837a1b3
Renan Martins <renanmartins1999@hotmail.com>
2024-02-19 06:29:46 -0300

    [FEAT] Create loans view model

---
+++ src/LibraryManager.API/ViewModels/CreateLoanViewModel.cs
@@ -0,0 +1,20 @@
+using LibraryManager.API.Models;
+
+namespace LibraryManager.API.ViewModels
+{
+    public class CreateLoanViewModel
+    {
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public DateTime LoanDate { get; set; }
+
+        public static Loan ToEntity(CreateLoanViewModel loan)
+        {
+            return new Loan {
+                UserId = loan.UserId,
+                BookId = loan.BookId,
+                LoanDate = loan.LoanDate
+            };
+        }
+    }
+}
