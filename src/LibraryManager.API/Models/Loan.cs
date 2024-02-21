51cc540f0574bc0bf789b3799a02f69612047c9e
Renan Martins <renanmartins1999@hotmail.com>
2024-02-20 21:37:37 -0300

    [FEAT] New IsExpired prop in Loan

--- src/LibraryManager.API/Models/Loan.cs
+++ src/LibraryManager.API/Models/Loan.cs
@@ -6,5 +6,6 @@ namespace LibraryManager.API.Models
         public int UserId { get; set; }
         public int BookId { get; set; }
         public DateTime LoanDate { get; set; }
+        public bool IsExpired => LoanDate < DateTime.Today;
     }
 }
