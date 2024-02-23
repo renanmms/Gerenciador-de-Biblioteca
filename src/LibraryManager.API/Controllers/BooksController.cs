06538fe9bbe942a9257280e76ecee8af9b374363
Renan Martins <renanmartins1999@hotmail.com>
2024-02-22 22:24:50 -0300

    [FEAT] Using book service in controller

--- src/LibraryManager.API/Controllers/BooksController.cs
+++ src/LibraryManager.API/Controllers/BooksController.cs
@@ -1,3 +1,4 @@
+using LibraryManager.Application.Services.Interfaces;
 using LibraryManager.Application.ViewModels;
 using LibraryManager.Infrastructure.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -8,23 +9,23 @@ namespace LibraryManager.API.Controllers
     [Route("api/[controller]")]
     public class BooksController : ControllerBase
     {
-        private readonly IBookRepository _bookRepository;
-        public BooksController(IBookRepository bookRepository)
+        private readonly IBookService _bookService;
+        public BooksController(IBookService bookService)
         {
-            _bookRepository = bookRepository;
+            _bookService = bookService;
         }
 
         [HttpGet("get/{id}")]
         public IActionResult GetById(int id)
         {
-            var book = _bookRepository.GetById(id);
+            var book = _bookService.GetById(id);
             return Ok(book);
         }
 
         [HttpGet("get")]
         public IActionResult GetAll(string query = "")
         {
-            var books = _bookRepository.GetAll(query);
+            var books = _bookService.GetAll(query);
             return Ok(books);
         }
 
@@ -32,7 +33,7 @@ namespace LibraryManager.API.Controllers
         public IActionResult Post(CreateBookViewModel book)
         {
             try{
-                var id = _bookRepository.Create(CreateBookViewModel.ToEntity(book));
+                var id = _bookService.Create(book);
                 return Created(nameof(GetById), new {id = id});
 
             }catch(Exception ex){
@@ -45,54 +46,12 @@ namespace LibraryManager.API.Controllers
         {
             try
             {
-                _bookRepository.Delete(id);
+                _bookService.Delete(id);
                 return Ok("Book deleted successfully");
             }
             catch (Exception ex)
             {
                 return BadRequest();
-            }
-        }
-
-        [HttpGet("get/loans/{bookId}")]
-        public IActionResult GetLoansByBookId(int bookId)
-        {
-            var loans = _bookRepository.GetLoansByBookId(bookId);
-            return Ok(loans);
-        }
-
-        [HttpPost("create/loan")]
-        public IActionResult CreateLoan(CreateLoanViewModel loan)
-        {
-            try
-            {
-                var bookId = _bookRepository.CreateLoan(CreateLoanViewModel.ToEntity(loan));
-                return Created(nameof(GetLoansByBookId), new {id = bookId});
-            }
-            catch (Exception ex)
-            {       
-                throw;
-            }
-        }
-
-        [HttpDelete("return")]
-        public IActionResult Return(ReturnBookViewModel model)
-        {
-            try
-            {
-                var loan = _bookRepository.GetLoan(model.UserId, model.BookId);
-
-                if(loan.IsExpired)
-                {
-                    return BadRequest("Loan is expired! Please contact the library to further details.");
-                }
-
-                _bookRepository.Return(model.UserId, model.BookId);
-                return Ok("Book returned successfully");
-            }
-            catch (Exception ex)
-            {
-                throw;
             }
         }
     }
