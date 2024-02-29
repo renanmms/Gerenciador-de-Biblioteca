ea64b137f32b31902ea8484afd779fa938e73afa
Renan Martins <renanmartins1999@hotmail.com>
2024-02-29 05:56:24 -0300

    [FEAT] Adding role and password to user

--- src/LibraryManager.Core/Entities/User.cs
+++ src/LibraryManager.Core/Entities/User.cs
@@ -5,6 +5,8 @@ namespace LibraryManager.Core.Entities
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Role { get; set; }
         public ICollection<Book> Books { get; set; }
     }
 }
